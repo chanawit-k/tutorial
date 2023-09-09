bf25cd666b43bc835fce5a69e8b20e609169154b
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2023-09-09 10:29:56 +0700

    - add css + component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
-import React from "react";
-import ReactDOM  from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
 
 // ต้องขึ่้นต้นตัวใหญ่เสมอ
-function Greeting(){
-    return <h2>My first component</h2>;
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
 }
 
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting />)
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/41p6OBQLfXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt="Best seller"
+  />
+)
+const Title = () => <h2>The PAct</h2>
+const Author = () => <h2>Author Name</h2>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Booklist />)
