8dd8f548d231a6fdf78e8d273f932e56c0e1e12e
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2023-10-02 19:15:56 +0700

    - initail props

--- src/index.js
+++ src/index.js
@@ -2,29 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+const title = 'the pact'
+const author = 'chanawit'
+const img =
+  'https://m.media-amazon.com/images/I/41p6OBQLfXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'
 
 // ต้องขึ่้นต้นตัวใหญ่เสมอ
 const Booklist = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book title={title} author={author} img={img} />
+      <Book title={title} author={author} img={img} />
     </section>
   )
 }
 
-const Book = () => {
-  const title = 'the pact'
-  const author = 'chanawit'
+const Book = ({ title, author, img }) => {
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/41p6OBQLfXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt="Best seller"
-      />
+      <img src={img} alt="Best seller" />
       <h2>{title}</h2>
       <h2>{author}</h2>
     </article>
