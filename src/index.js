f220695a94bb61772249272c2c641a83e7ed77bc
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2023-10-03 19:00:57 +0700

    use '...' copy element

--- src/index.js
+++ src/index.js
@@ -4,47 +4,42 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 const book_list = [
   {
+    id: '1',
     img: 'https://m.media-amazon.com/images/I/91ke43dIxkL._SY466_.jpg',
     title: 'Iron Flame',
     author: 'Rebecca Yarros',
   },
   {
+    id: '2S',
     img: 'https://m.media-amazon.com/images/I/41p6OBQLfXL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'the pact',
     author: 'Christ Evan',
   },
 ]
 
-const friends = ['john', 'peter', 'anna']
-const newFriends = [...friends, 'susan']
-console.log(friends)
-console.log(newFriends)
-const someObject = {
-  name: 'john',
-  job: 'developer',
-}
-// COPY NOT A REFERENCE !!!!
-const newObject = { ...someObject, location: 'florida' }
-console.log(someObject)
-console.log(newObject)
-
 // ต้องขึ่้นต้นตัวใหญ่เสมอ
 const Booklist = () => {
   return (
     <section className="booklist">
       {book_list.map((book) => {
-        return <Book book={book}></Book>
+        return (
+          <Book {...book} key={book.title}>
+            <p>test children</p>
+          </Book>
+        )
       })}
     </section>
   )
 }
 
-const Book = ({ book: { img, title, author } }) => {
+const Book = (props) => {
+  const { img, title, author, children } = props
   return (
     <article className="book">
       <img src={img} alt="Best seller" />
       <h2>{title}</h2>
       <h2>{author}</h2>
+      {children}
     </article>
   )
 }
